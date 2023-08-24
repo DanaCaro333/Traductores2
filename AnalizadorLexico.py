b3fe4ee8c16a00d324d6bd0f2834ffd0715944f0
Dana Ramirez <dana.caro.ram@gmail.com>
2023-08-24 16:33:33 -0600

    actualizacion main

--- AnalizadorLexico.py
+++ AnalizadorLexico.py
@@ -5,12 +5,13 @@ class Type:
     MULT = 2
     PESOS = 3
     ENTERO = 4
+    REAL = 5
 
 
 class Lexico(object):
-    def __init__(self, fuente) -> None:
-        self.__fuente = fuente
-        self.__ind = 0
+    def __init__(self) -> None:
+        self.__fuente = ""
+        self.__ind = -1
         self.__continua = True
         self.__c = ''
         self.__estado = 0
@@ -19,18 +20,18 @@ class Lexico(object):
         self.tipo = 0
 
     def __sigCaracter(self):
-        if self.terminado(self):
+        if self.terminado():
             return '$'
         else:
-            return self.__fuente[self.__ind+1]
+            self.__ind = self.__ind + 1
+            return self.__fuente[self.__ind]
 
     def __sigEstado(self, estado):
-        self.__sigEstado = estado
+        self.__estado = estado
         self.simbolo += self.__c
-        return
 
     def __aceptacion(self, estado):
-        self.__sigEstado(self, estado)
+        self.__sigEstado(estado)
         self.__continua = False
 
     def __esLetra(self, c):
@@ -48,7 +49,7 @@ class Lexico(object):
         return
 
     def entrada(self, fuente):
-        self.__ind = 0
+        self.__ind = -1
         self.__fuente = fuente
 
     def tipoAcad(self, tipo):
@@ -69,6 +70,12 @@ class Lexico(object):
         elif self.tipo == Type.ENTERO:
             cad = "Entero"
 
+        elif self.tipo == Type.REAL:
+            cad = "Real"
+
+        elif self.tipo == Type.ERROR:
+            cad = "Error en el token"
+
         return cad
 
     def sigSimbolo(self):
@@ -77,21 +84,49 @@ class Lexico(object):
         self.simbolo = ""
 
         while(self.__continua):
-            self.__c = self.__sigCaracter(self)
+            self.__c = self.__sigCaracter()
 
             if self.__estado == 0:
-                if self.__c == '+' or self.__c == '-':
-                    self.__aceptacion(self, 2)
-                elif self.__c == '$':
-                    self.__aceptacion(self, 3)
+                if self.__esLetra(self.__c):
+                    self.__sigEstado(1)
+                elif self.__esDigito(self.__c):
+                    self.__sigEstado(3)
+                else:
+                    self.__continua = False
+            elif self.__estado == 1:
+                if self.__esLetra(self.__c) or self.__esDigito(self.__c):
+                    self.__sigEstado(1)
+                elif self.__c == "*":
+                    self.__aceptacion(2)
+                else:
+                    self.__continua = False
+            elif self.__estado == 3:
+                if self.__esDigito(self.__c):
+                    self.__sigEstado(3)
+                elif self.__c == ".":
+                    self.__sigEstado(4)
+                else:
+                    self.__continua = False
+            elif self.__estado == 4:
+                if self.__esDigito(self.__c):
+                    self.__sigEstado(4)
+                elif self.__c == "+":
+                    self.__aceptacion(5)
+                else:
+                    self.__continua = False
 
-        if self.__estado == 2:
-            self.tipo = Type.SUMA
+        if self.__estado == 5:
+            self.tipo = Type.REAL
+        elif self.__estado == 2:
+            self.tipo = Type.IDENTIFICADOR
+        elif self.__estado == 0:
+            self.tipo = Type.PESOS
+            self.simbolo = self.__c
         else:
+            self.simbolo += self.__c
             self.tipo = Type.ERROR
 
         return self.tipo
 
     def terminado(self):
-        print(self.__fuente)
-        return self.__ind >= self.__fuente.len()
+        return self.__ind >= len(self.__fuente)-1
--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 import AnalizadorLexico
 
-prueba = AnalizadorLexico.Lexico("hola+mundo")
+prueba = AnalizadorLexico.Lexico()
+prueba.entrada("a23*2.0+234*")
+
+print("Resultado del analisis lexico\n\n")
+
+while(prueba.simbolo != "$"):
+    prueba.sigSimbolo()
+    if(prueba.simbolo != "$"):
+        print(prueba.simbolo+"\t\t"+prueba.tipoAcad(prueba.tipo)+"\n")
+    else:
+        print(prueba.tipoAcad(prueba.tipo)+"\n")
