e6402e72e8f4dab2f8846d5d2d6ca71b6c67ba9d
Dana Ramirez <dana.caro.ram@gmail.com>
2023-11-28 19:37:09 -0600

    ProyectoFinal

--- ProyectoFinal/ensamblador.asm
+++ ProyectoFinal/ensamblador.asm
@@ -1,15 +1,15 @@
-section .data  
-	primr: db  "La impresion es := %lf",10,0 
-	section .bss 
+section .data 
+	primr: db  "Resultado := %lf",10,0
+	section .bss
 	resp: resq 2
-section .text 
+section .text
 
 	extern printf, scanf
 global menu
 menu:
- 	PUSH rbp 
-	MOV rbp, rsp 
-	SUB rsp, 48 
+ 	PUSH rbp
+	MOV rbp, rsp
+	SUB rsp, 48
 		MOV WORD [rbp -4] , 2
 	PUSH qword[rbp -4]
 	FILD dword[rsp]
@@ -19,8 +19,7 @@ menu:
 	MOV rdi, primr
 	MOV rdi, primr
 	call printf WRT ..plt
-	ADD rsp, 48 
-	MOV rsp, rbp 
-	POP rbp 
-	ret 
-	
+	ADD rsp, 48
+	MOV rsp, rbp
+	POP rbp
+	ret
